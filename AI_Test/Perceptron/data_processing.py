288ef89560df9a5cb2b59d64b5685579fd387ee6
Jiaming <424366965@qq.com>
2020-03-02 23:32:04 +0800

    We can train and predict images now

--- AI_Test/Perceptron/data_processing.py
+++ AI_Test/Perceptron/data_processing.py
@@ -9,9 +9,9 @@ def convert_img_to_csv(img_dir):
         writer = csv.writer(f)
         writer.writerow(column_name)
         #该目录下有9个目录,目录名从0-9
-
+        img_dir_circle = img_dir + 'circles'
         #获取目录的路径
-        img_temp_dir = os.path.join(img_dir)
+        img_temp_dir = os.path.join(img_dir_circle)
         #获取该目录下所有的文件
         img_list = os.listdir(img_temp_dir)
         #遍历所有的文件名称
@@ -29,8 +29,26 @@ def convert_img_to_csv(img_dir):
                 #将图片数据写入到csv文件中
                 writer.writerow(row_data)
 
+        img_dir_square = img_dir + 'squares'
+        img_temp_dir = os.path.join(img_dir_square)
+        #获取该目录下所有的文件
+        img_list = os.listdir(img_temp_dir)
+        #遍历所有的文件名称
+        for img_name in img_list:
+            #判断文件是否为目录,如果为目录则不处理
+            if not os.path.isdir(img_name):
+                #获取图片的路径
+                img_path = os.path.join(img_temp_dir,img_name)
+                #因为图片是黑白的，所以以灰色读取图片
+                img = cv2.imread(img_path,cv2.IMREAD_GRAYSCALE)
+                #图片标签
+                row_data = [1]
+                #获取图片的像素
+                row_data.extend(img.flatten())
+                #将图片数据写入到csv文件中
+                writer.writerow(row_data)
 
 if __name__ == "__main__":
     #将该目录下的图片保存为csv文件
     #convert_img_to_csv(r"F:/DockerProjet/DI5-S10-ProjetLibre-IA/AI_Test/Perceptron/circles")
-    convert_img_to_csv("./circles")
+    convert_img_to_csv("../kaggleData/basicshapes/")
--- AI_Test/Perceptron/main.py
+++ AI_Test/Perceptron/main.py
@@ -1,2 +1,39 @@
-import AI_Test.Perceptron.Perceptron as perceptron_class
+
+from PIL import Image
 import numpy as np
+import pandas as pd
+from matplotlib.colors import ListedColormap
+import AI_Test.Perceptron.Perceptron as perceptron_class
+
+df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data', header=None)
+dataframe = pd.read_csv(r"D:\France\S10\Projet Libre\DI5-S10-ProjetLibre-IA\AI_Test\Perceptron\newtest.csv")
+
+#抽取出前100条样本，这正好是Setosa和Versicolor对应的样本，我们将Versicolor对应的数据作为类别1，Setosa对应的作为-1。
+# 对于特征，我们抽取出sepal length和petal length两维度特征，然后用散点图对数据进行可视化
+
+y = dataframe.iloc[0:200, 0].values
+y = np.where(y == 0, -1, 1)
+X = dataframe.iloc[0:200, 1:785].values
+print(X[0][0])
+for i in range (200):
+    for j in range(784):
+        X[i][j] = float(X[i][j])
+
+# y = df.iloc[0:100, 4].values
+# y = np.where(y == 'Iris-setosa', -1, 1)
+# X = df.iloc[0:100, [0, 2]].values
+
+# image = Image.open("D:/France/S10/Projet Libre/DI5-S10-ProjetLibre-IA/AI_Test/kaggleData/basicshapes/circles/drawing(1).png") # 用PIL中的Image.open打开图像
+# image = image.convert('L')
+# image_arr = np.array(image) # 转化成numpy数组
+# images=[]
+# images.append(image_arr)
+# images = np.array(images)
+# label =[1]
+#这里是对于感知机模型进行训练
+ppn = perceptron_class.Perceptron(eta=0.1, n_iter=10)
+ppn.fit(X, y)
+
+Z = ppn.predict(X[10])
+print(y[10])
+print(Z)
